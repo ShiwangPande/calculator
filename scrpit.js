c9739c256735e4960e06de288ce3785cf66167fe
shiwang pandey <97517610+shiwangPandey@users.noreply.github.com>
2022-07-14 00:09:46 +0530

    Delete scrpit.js

--- scrpit.js
+++
@@ -1,6 +0,0 @@
-function copyToClipBoard() {
-    var content = document.getElementById("textArea");
-    content.select();
-    document.exeCommand('copy');
-    alert("Successfully Copied!");
-    }
